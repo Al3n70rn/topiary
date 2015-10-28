849a2d64d86d645b99f2bb49db5c794765ce8dc7
B. Arman Aksoy <arman@aksoy.org>
2015-10-28 12:47:52 -0400

    add configuration for RTD service

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+# Configuration file for ReadTheDocs service
+
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['matplotlib']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
