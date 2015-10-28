a6a7cc4ddf5157b425f29d7f4a33b28ead0377ca
B. Arman Aksoy <arman@aksoy.org>
2015-10-28 13:25:52 -0400

    mock varcode and sigh

--- conf.py
+++ conf.py
@@ -8,5 +8,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot', 'scikit-bio']
+MOCK_MODULES = ['varcode']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
